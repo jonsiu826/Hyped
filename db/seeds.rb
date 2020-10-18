1cadf258d76a219c54f918c44c60e0179d25ceae
jsiu <jonsiu826@gmail.com>
2020-10-17 22:17:08 -0700

    css to kobe pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ u6 = User.create!(
     password: 'password'
 )
 
-product1 = Product.create!(
+jordan1_1 = Product.create!(
    name: "Air Jordan 1 Retro High OG 'Chicago' 2015", 
    brand: "Air Jordan", 
    release_date: "2015-05-29", 
@@ -58,7 +58,7 @@ product1 = Product.create!(
    description: "The Air Jordan 1 Retro High OG 'Chicago' 2015 colorway was designed to avoid Jordan's $5,000-per game fines from the NBA after the original black and red colorway was banned. The 'Chicago' was first retroed in 1994 and stayed true to the 1985 design. Further retros were released in 2013 with small changes to the design, and again in 2015 with the original branding details: the Nike Air logo on the tongue and the original clean heel without the Jumpman logo."
 )
 
-product2 = Product.create!(
+jordan1_2 = Product.create!(
    name: "Air Jordan 1 Mid 'Pine Green'", 
    brand: "Air Jordan", 
    release_date: "2018-12-18", 
@@ -71,7 +71,7 @@ product2 = Product.create!(
    description: "The Air Jordan 1 Mid ‘Pine Green’ brings back the principal hue from a full-height Jordan 1 from September 2018. Here, a tumbled leather base in Sail is accented with Pine Green overlays at the toe, heel and collar. The two-tone design is interrupted by contrasting hits of black on the Swoosh, laces and nylon tongue, topped with a Jumpman-adorned woven tag."
 )
 
-product3 = Product.create!(
+jordan1_3 = Product.create!(
    name: "Air Jordan 1 Retro High OG 'Dark Mocha'", 
    brand: "Air Jordan", 
    release_date: "2020-10-31", 
@@ -84,7 +84,7 @@ product3 = Product.create!(
    description: "The Air Jordan 1 Retro High OG ‘Dark Mocha’ features a distinctive palette that calls to mind Travis Scott’s highly coveted Air Jordan 1 collaboration from 2019. The upper features an off-white leather base with contrasting black leather overlays on the eyestay and toe. Brown nubuck is utilized on the heel overlay and collar flap, accented with a classic Wings logo on the lateral side. A leather tag with Nike Air branding adorns the nylon tongue."
 )
 
-product4 = Product.create!(
+jordan1_4 = Product.create!(
    name: "Air Jordan 1 Retro High OG 'Obsidian'", 
    brand: "Air Jordan", 
    release_date: "2019-08-31", 
@@ -97,7 +97,7 @@ product4 = Product.create!(
    description: "The Air Jordan 1 Retro High OG is a work of nostalgia, modernizing the 1985 icon's distinctive elements to honor Michael Jordan's legendary career arc. Debuted in August 2019, this 'Obsidian' version tributes MJ's college alma mater through a team-inspired, color block palette on the leather upper. Matching branding notes and Swooshes play their part in the homage. The Nike Air infused midsole and circular rubber outsole supply classic comfort and traction."
 )
 
-product5 = Product.create!(
+jordan1_5 = Product.create!(
    name: "Travis Scott x Air Jordan 1 Retro High OG 'Mocha'", 
    brand: "Air Jordan", 
    release_date: "2019-05-11", 
@@ -110,7 +110,32 @@ product5 = Product.create!(
    description: "The Travis Scott x Air Jordan 1 Retro High features a new look on the iconic silhouette, thanks to an oversized backward-facing Swoosh on the lateral side. A traditionally oriented Swoosh graces the medial side of the upper, which is built with a blend of white leather and brown suede. Additional unique details include a double-layer construction on the collar and Scott’s crudely drawn face logo embossed on the heel."
 )
 
-product6 = Product.create!(
+jordan1_6 = Product.create!(
+   name: "Air Jordan 1 Retro High OG 'Crimson Tint'",
+   brand: "Air Jordan", 
+   release_date: "2019-04-11", 
+   colorway: "Black/Crimson Tint-Hyper Pink-White", 
+   technology: "Air", 
+   category: "Lifestyle", 
+   silhouette: "Air Jordan 1", 
+   main_color: "Pink", 
+   designer:"Peter Moore",
+   description: "The Air Jordan 1 Retro High OG ‘Crimson Tint’ showcases a simple two-tone design, executed on an all-leather build that incorporates both tumbled leather and smooth full-grain leather. The latter is used on the toe box, Swoosh, collar and heel panel, all finished in Crimson Tint. The black tumbled leather overlays are matched by black laces and a black nylon tongue, topped with a woven Nike tag."
+)
+
+jordan1_7 = Product.create!(
+   name: "Air Jordan 1 Retro High OG 'Turbo Green'",
+   brand: "Air Jordan", 
+   release_date: "2019-04-11", 
+   colorway: "Turbo Green/White-Light Smoke Grey-Sail", 
+   technology: "Air", 
+   category: "Lifestyle", 
+   silhouette: "Air Jordan 1", 
+   main_color: "Green", 
+   designer:"Peter Moore",
+   description: "The Air Jordan 1 Retro High OG ‘Turbo Green’ complements the silhouette’s clean lines with a classic two-tone design. A textured white leather base is contrasted by soft suede overlays in Turbo Green. The latter design element is burnished to give off a vintage aesthetic, a quality that’s further enhanced by a midsole finished in Off-White Sail. In addition to a woven Nike Air tongue tag in purple and yellow, exterior branding also includes a line of text at the base of the quarter panel that reads ‘SP 19 Air Jordan 1 High OG.’"
+)
+kobe5_1 = Product.create!(
    name: "Zoom Kobe 5 Protro 'DeMar DeRozan' PE", 
    brand: "Nike", 
    release_date: "2020-09-18", 
@@ -123,7 +148,7 @@ product6 = Product.create!(
    description: "A special colorway made for the NBA athlete and noted sneaker enthusiast, the Nike Zoom Kobe 5 Protro ‘P.J. Tucker’ PE features tonal animal prints throughout the lightweight synthetic upper, finished in neutral grey and off-white tones and supported by Flywire technology. Bright red mesh stands out on the collar, contrasted by a black heel counter marked with Kobe Bryant’s signature. A dot matrix-like design fills in the signature Swoosh on the sneaker’s quarter panel."
 )
 
-product7 = Product.create!(
+kobe5_2 = Product.create!(
    name: "Zoom Kobe 5 Protro '5 Rings'", 
    brand: "Air Jordan", 
    release_date: "2020-10-15", 
@@ -136,7 +161,7 @@ product7 = Product.create!(
    description: "The Nike Zoom Kobe 5 Protro ‘5 Rings’ brings back a celebratory Kobe 5 colorway that originally launched in summer 2010 in recognition of the Mamba’s fifth championship with the Los Angeles Lakers. The low-top makes use of a simple palette in Concord and Midwest Gold, executed on a streamlined upper covered in various graphics centered around the number 5. A gradient two-tone midsole packs Zoom Air cushioning in both the heel and forefoot."
 )
 
-product8 = Product.create!(
+kobe5_3 = Product.create!(
    name: "Zoom Kobe 5 Protro 'Big Stage'", 
    brand: "Nike", 
    release_date: "2020-08-23", 
@@ -149,7 +174,7 @@ product8 = Product.create!(
    description: "The Nike Kobe 5 Protro 'Big Stage' gets its name from the shoe that Kobe Bryant wore during the 2010 NBA Finals, which culminated in the Mamba clinching his fifth and final championship with the Lakers. The design combines ‘home’ and ‘away’ editions of the ‘Big Stage’ Kobe 5 with graphic elements borrowed from the ‘Parade’ PE, which cover the lateral side of the left shoe and medial side of the right. Contrasting pops of metallic gold appear on the sneaker’s heel and branding hits."
 )
 
-product9 = Product.create!(
+kobe5_4 = Product.create!(
    name: "Zoom Kobe 5 Protro 'EYBL'", 
    brand: "Nike", 
    release_date: "2020-08-29", 
@@ -162,7 +187,7 @@ product9 = Product.create!(
    description: "The Nike Zoom Kobe 5 Protro ‘EYBL’ is taken from a special 2020 collection that launched as part of Mamba Week, a weeklong celebration of Kobe Bryant’s legacy that kicked off on the athlete’s birthday, August 23. The low-top draws inspiration from Greek mythology, featuring a scaly texture on a dark green synthetic upper that conjures images of the villainous Medusa. Nike’s EYBL logo adorns the tongue of the left shoe, while Kobe’s personal logo graces the opposite tongue."
 )
 
-product10 = Product.create!(
+kobe5_5 = Product.create!(
    name: "Zoom Kobe 5 Protro 'P.J. Tucker' PE", 
    brand: "Nike", 
    release_date: "2020-09-25", 
@@ -176,31 +201,37 @@ product10 = Product.create!(
 )
 
 file1 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/jordan1_chicago.jpeg")
-product1.photo.attach(io: file1, filename: "img_#{product1.id}.jpg")
+jordan1_1.photo.attach(io: file1, filename: "img_#{jordan1_1.id}.jpg")
 
 file2 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/jordan1_green.jpeg")
-product2.photo.attach(io: file2, filename: "img_#{product2.id}.jpg")
+jordan1_2.photo.attach(io: file2, filename: "img_#{jordan1_2.id}.jpg")
 
 file3 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/jordan1_mocha.jpeg")
-product3.photo.attach(io: file3, filename: "img_#{product3.id}.jpg")
+jordan1_3.photo.attach(io: file3, filename: "img_#{jordan1_3.id}.jpg")
 
 file4 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/jordan1_obsidian.jpeg")
-product4.photo.attach(io: file4, filename: "img_#{product4.id}.jpg")
+jordan1_4.photo.attach(io: file4, filename: "img_#{jordan1_4.id}.jpg")
 
 file5 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/Jordan1_TravisScott.jpeg")
-product5.photo.attach(io: file5, filename: "img_#{product5.id}.jpg")
+jordan1_5.photo.attach(io: file5, filename: "img_#{jordan1_5.id}.jpg")
 
 file6 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/kobe_demar_PE.jpeg")
-product6.photo.attach(io: file6, filename: "img_#{product6.id}.jpg")
+kobe5_1.photo.attach(io: file6, filename: "img_#{kobe5_1.id}.jpg")
 
 file7 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/Kobe_5rings.jpeg")
-product7.photo.attach(io: file7, filename: "img_#{product7.id}.jpg")
+kobe5_2.photo.attach(io: file7, filename: "img_#{kobe5_2.id}.jpg")
 
 file8 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/kobe_gold.jpeg")
-product8.photo.attach(io: file8, filename: "img_#{product8.id}.jpg")
+kobe5_3.photo.attach(io: file8, filename: "img_#{kobe5_3.id}.jpg")
 
 file9 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/Kobe_5eyeybl.jpeg")
-product9.photo.attach(io: file9, filename: "img_#{product9.id}.jpg")
+kobe5_4.photo.attach(io: file9, filename: "img_#{kobe5_4.id}.jpg")
 
 file10 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/kobe_tucker_PE.jpeg")
-product10.photo.attach(io: file10, filename: "img_#{product10.id}.jpg")
+kobe5_5.photo.attach(io: file10, filename: "img_#{kobe5_5.id}.jpg")
+
+file11 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/Jordan1_crimsontint.jpeg")
+jordan1_6.photo.attach(io: file11, filename: "img_#{jordan1_6.id}.jpg")
+
+file12 = open("https://hyped-shoes-seed.s3-us-west-1.amazonaws.com/jordan1_turbogreen.jpeg")
+jordan1_7.photo.attach(io: file12, filename: "img_#{jordan1_7.id}.jpg")
--- frontend/components/products/product_index.jsx
+++ frontend/components/products/product_index.jsx
@@ -18,8 +18,10 @@ class ProductIndex extends React.Component {
         })
         return (
             <div>
-                <div className="kobe-logo">
+                <div className="kobe-logo-container">
                     <img className="kobe-image" src={Kobe1} />
+                    <h1 className="kobe-text">LEAVE YOUR LEGACY</h1>
+                    {/* <button className="kobe-button">Shop Kobes</button> */}
                 </div>
                 <ul className="product-ul">
                     {products}
