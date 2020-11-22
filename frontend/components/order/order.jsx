e99982142d56a046ef79a3d287dc99e813cd2a19
jsiu <jonsiu826@gmail.com>
2020-11-22 09:57:35 -0800

    adding to order modal

--- frontend/components/order/order.jsx
+++ frontend/components/order/order.jsx
@@ -12,10 +12,22 @@ class Order extends React.Component {
          
         return (
             <div className="order-container">
-                <h1 className="order-title">THANK YOU FOR SHOPPING AT HYPED</h1>
-                <p className="order-message">Your Order is on the Way!(#C00287300952)</p>
-                <p className="order-desc"> Hang tight, {this.props.currentUser.username}! We're working to get it packed up and out the door. We may send your order in more than one shipment, and if we do, we'll send a shipping confirmation email as each shipment goes out.</p>
-                <button onClick={this.props.closeModal}>Continue Shopping</button>
+                <div>
+                    <h1 className="order-title">THANK YOU FOR SHOPPING AT HYPED</h1>
+                    <p className="order-message">Hang tight, {this.props.currentUser.username}!</p>
+                    <p className="order-number">Your Order is on the Way! (#C00287300952)</p>
+                    <p className="order-desc"> We're working to get it packed up and out the door. We may send your order in more than one shipment, and if we do, we'll send a shipping confirmation email as each shipment goes out.</p>
+                    <button className="order-button"onClick={this.props.closeModal}>Continue Shopping</button>
+                </div>
+                <div>
+                    <div>
+                        <p>Billing Address</p>
+                    </div>
+                    <div>  
+                        <p>Shipping Address</p>
+                    </div>
+                </div>
+
             </div>
             )
     }
