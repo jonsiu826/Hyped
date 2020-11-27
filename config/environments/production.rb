ccfe7fc779ce6617dfa4743f7b9032593f6f232f
jsiu <jonsiu826@gmail.com>
2020-11-27 12:31:35 -0800

    adding to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+  config.assets.enabled = true 
+  config.serve_static_assets = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
