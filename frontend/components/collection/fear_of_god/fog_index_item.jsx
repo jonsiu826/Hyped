37a0c137b290547e3d2553dca568ef7be6548d28
jsiu <jonsiu826@gmail.com>
2020-11-27 20:08:44 -0800

    adding changes to styling

--- frontend/components/collection/fear_of_god/fog_index_item.jsx
+++ frontend/components/collection/fear_of_god/fog_index_item.jsx
@@ -9,16 +9,16 @@ class FogIndexItem extends React.Component{
         
          if (this.props.product.silhouette === "Air Fear of God 1"){
              return (
-                 <div className="sneaker-container">
-                     <li className="fog-indexitem">
-                             <div className="fog-release-date">{this.props.product.release_date}</div>
-                       
-                         <Link className="fog-sneaker-info" to={`/products/${this.props.product.id}`}>
-                             <img src={this.props.product.photo_url} className="fog-image"/>
-                             <div className="fog-name">{this.props.product.name}</div>
-                          </Link>
-                     </li>       
-                 </div>
+                <div className="sneaker-container">
+                    <li className="sneaker-indexitem">
+                            <div className="release-date">{this.props.product.release_date}</div>
+                      
+                        <Link className="sneaker-info" to={`/products/${this.props.product.id}`}>
+                            <img src={this.props.product.photo_url} className="product-image"/>
+                            <div className="sneaker-name">{this.props.product.name}</div>
+                         </Link>
+                    </li>       
+                </div>
              )
 
          } else {
